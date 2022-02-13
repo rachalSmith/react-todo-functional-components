0d8c7cbbd735643b2c313666da67ed468ba3a790
rachalsmith <66318063+rachalSmith@users.noreply.github.com>
2022-02-13 14:57:23 +0000

    Delete src/components directory

--- src/components/__test__/Header.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import Header from '../Header';
-
-it('should render the header', () => {
-    render(<Header />);
-    const headerElement = screen.getByRole('heading');
-    expect(headerElement).toBeVisible();
-  });
--- src/components/__test__/ToDo.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import ToDo from '../ToDo';
-
-it('should render with a delete button', () => {
-    render(<ToDo />)
-    const deleteButton = screen.queryByRole('button', { name: "x"});
-    expect(deleteButton).not.toBeInTheDocument()
-})
